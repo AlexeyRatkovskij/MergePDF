17159199b845fc7f11fde322105ac77cd9a4c086
Alexey Ratkovskiy <alexwells.corp@gmail.com>
2024-03-16 16:35:39 +0300

    Добавьте файлы проекта.

---
+++ MergePDF/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace MergePDF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.DropPanel = new System.Windows.Forms.Panel();
+            this.DropImage = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.FileList = new System.Windows.Forms.ListView();
+            this.listHeaderNum = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listHeaderFilename = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.BtnMerge = new System.Windows.Forms.Button();
+            this.DropPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DropImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DropPanel
+            // 
+            this.DropPanel.AllowDrop = true;
+            this.DropPanel.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.DropPanel, "DropPanel");
+            this.DropPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DropPanel.Controls.Add(this.DropImage);
+            this.DropPanel.Name = "DropPanel";
+            this.DropPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.evDragPanelDrop);
+            this.DropPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.evDragPanelEnter);
+            this.DropPanel.DragOver += new System.Windows.Forms.DragEventHandler(this.evDragPanelOver);
+            this.DropPanel.DragLeave += new System.EventHandler(this.evDragPanelLeave);
+            // 
+            // DropImage
+            // 
+            resources.ApplyResources(this.DropImage, "DropImage");
+            this.DropImage.Name = "DropImage";
+            this.DropImage.TabStop = false;
+            // 
+            // FileList
+            // 
+            this.FileList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listHeaderNum,
+            this.listHeaderFilename});
+            this.FileList.HideSelection = false;
+            resources.ApplyResources(this.FileList, "FileList");
+            this.FileList.Name = "FileList";
+            this.FileList.UseCompatibleStateImageBehavior = false;
+            this.FileList.View = System.Windows.Forms.View.Details;
+            // 
+            // listHeaderNum
+            // 
+            resources.ApplyResources(this.listHeaderNum, "listHeaderNum");
+            // 
+            // listHeaderFilename
+            // 
+            resources.ApplyResources(this.listHeaderFilename, "listHeaderFilename");
+            // 
+            // btnUp
+            // 
+            this.btnUp.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnUp, "btnUp");
+            this.btnUp.Name = "btnUp";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnDown, "btnDown");
+            this.btnDown.Name = "btnDown";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.BtnDelete, "BtnDelete");
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.UseVisualStyleBackColor = true;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // BtnMerge
+            // 
+            this.BtnMerge.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.BtnMerge, "BtnMerge");
+            this.BtnMerge.Name = "BtnMerge";
+            this.BtnMerge.UseVisualStyleBackColor = true;
+            this.BtnMerge.Click += new System.EventHandler(this.BtnMerge_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.BtnMerge);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.FileList);
+            this.Controls.Add(this.DropPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.DropPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DropImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel DropPanel;
+        private System.Windows.Forms.PictureBox DropImage;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView FileList;
+        private System.Windows.Forms.ColumnHeader listHeaderNum;
+        private System.Windows.Forms.ColumnHeader listHeaderFilename;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.Button BtnMerge;
+    }
+}
+
---
+++ MergePDF/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using ToolTip = System.Windows.Forms.ToolTip;
+
+namespace MergePDF
+{
+    public partial class MainForm : Form
+    {
+        private List<PDF_File> files;
+        private ToolTip toolTip;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.files = new List<PDF_File>();
+            setDefaultImage();
+
+            toolTip = new ToolTip();
+            toolTip.AutoPopDelay = 5000; // Задержка перед автоматическим закрытием
+            toolTip.InitialDelay = 1000; // Задержка перед появлением подсказки
+            toolTip.ReshowDelay = 500; // Задержка перед повторным появлением подсказки
+            toolTip.ShowAlways = true; // Показывать подсказку даже когда форма не активна
+
+            setBaseTooltips();
+        }
+
+        private void setBaseTooltips ()
+        {
+            toolTip.SetToolTip(DropImage,   "Перенесите сюда .pdf файлы для склеивания"     + Environment.NewLine + "Так же можно перенести папку с .pdf файлами");
+            toolTip.SetToolTip(btnUp,       "Перенести .pdf файл вверх по очереди"          + Environment.NewLine + "P.S.: Можно выделить несколько .pdf файлов");
+            toolTip.SetToolTip(btnDown,     "Перенести .pdf файл вниз по очереди"           + Environment.NewLine + "P.S.: Можно выделить несколько .pdf файлов");
+            toolTip.SetToolTip(BtnDelete,   "Удалить .pdf файл из очереди на склеивание"    + Environment.NewLine + "P.S.: Можно выделить несколько .pdf файлов");
+            toolTip.SetToolTip(BtnMerge,    "Склеить .pdf файлы в порядке очереди");
+        }
+
+        private void CreateFileItem (string _path)
+        {
+            string[] splitPath = _path.Split('\\');
+            if (splitPath.Length == 0) return;
+
+            string filename = splitPath[splitPath.Length - 1];
+            string[] splitFilename = filename.Split('.');
+
+            string extension = splitFilename[splitFilename.Length - 1];
+
+            if (extension.ToLower() == "pdf")
+            {
+                this.files.Add(new PDF_File(this.files.Count + 1, _path, filename));
+            }
+        }
+
+        private void FileListFill()
+        {
+            FileList.Items.Clear();
+            FileList.FullRowSelect = true;
+            FileList.ShowItemToolTips = true;
+            FileListNumChecked();
+
+            foreach (var file in this.files)
+            {
+                ListViewItem item = new ListViewItem(file.Num.ToString());
+                item.SubItems.Add(file.FileName);
+                item.BackColor = Color.White;
+                item.ToolTipText = file.Path;
+
+                FileList.Items.Add(item); 
+            }
+            this.Controls.Add(FileList);
+        }
+
+        private void FileListNumChecked()
+        {
+            for (int i = 0; i < this.files.Count; i++)
+            {
+                this.files[i].Num = i + 1;
+            }
+        }
+
+        private void evDragPanelDrop(object sender, DragEventArgs e)
+        {
+            foreach (var obj in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                if (Directory.Exists(obj))
+                {
+                    string[] files = Directory.GetFiles(obj, "*.pdf", SearchOption.AllDirectories);
+                    foreach (var file in files) CreateFileItem(file);
+                }
+                else
+                {
+                    CreateFileItem(obj);
+                }
+            }
+            FileListFill();
+            setDefaultImage();
+        }
+
+        private void evDragPanelEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void evDragPanelLeave(object sender, EventArgs e)
+        {
+            setDefaultImage();
+        }
+
+        private void evDragPanelOver(object sender, DragEventArgs e)
+        {
+            setHoverImage();
+        }
+
+        private void setHoverImage ()
+        {
+            try
+            {
+                Image image = Properties.Resources.plus_hover_state;
+                DropImage.BackgroundImage = image;
+                DropImage.BackgroundImageLayout = ImageLayout.Zoom;
+                DropImage.BackColor = Color.Transparent;
+
+                Color color = ColorTranslator.FromHtml("#12000000");
+                DropPanel.BackColor = color;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}");
+            }
+        }
+
+        private void setDefaultImage()
+        {
+            try
+            {
+                Image image = Properties.Resources.plus_default_state;
+                DropImage.BackgroundImage = image;
+                DropImage.BackgroundImageLayout = ImageLayout.Zoom;
+                DropImage.BackColor = Color.Transparent;
+
+                Color color = ColorTranslator.FromHtml("#00000000");
+                DropPanel.BackColor = color;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки изображения: {ex.Message}");
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (FileList.Items.Count > 0)
+            {
+                foreach (ListViewItem item in FileList.SelectedItems)
+                {
+                    files.RemoveAll(file => file.Num == Convert.ToInt32(item.SubItems[0].Text));
+                }
+                FileListFill();
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (FileList.SelectedItems.Count == 0) return;
+
+            int firstIndex = Convert.ToInt32(FileList.SelectedItems[0].Text) - 1;
+            int len = FileList.SelectedItems.Count;
+
+            if (firstIndex == 0) return;
+        
+            List<int> selectedIndexList = new List<int>();
+
+            for (int currentIndex = firstIndex; currentIndex < firstIndex + len; currentIndex++)
+            {
+                PDF_File currentItem = files[currentIndex];
+                files.RemoveAt(currentIndex);
+                files.Insert(currentIndex - 1, currentItem);
+
+                selectedIndexList.Add(currentIndex - 1);
+            }
+            FileListFill();
+
+            foreach (int selectedIndex in selectedIndexList)
+            {
+                FileList.Items[selectedIndex].Selected = true;
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (FileList.SelectedItems.Count == 0) return;
+
+            int lastIndex = Convert.ToInt32(FileList.SelectedItems[FileList.SelectedItems.Count - 1].Text) - 1;
+            int len = FileList.SelectedItems.Count;
+
+            if (lastIndex == files.Count - 1) return;
+
+            List<int> selectedIndexList = new List<int>();
+
+            for (int currentIndex = lastIndex; currentIndex > lastIndex - len; currentIndex--)
+            {
+                PDF_File currentItem = files[currentIndex];
+                files.RemoveAt(currentIndex);
+                files.Insert(currentIndex + 1, currentItem);
+
+                selectedIndexList.Add(currentIndex + 1);
+            }
+            FileListFill();
+
+            foreach (int selectedIndex in selectedIndexList)
+            {
+                FileList.Items[selectedIndex].Selected = true;
+            }
+        }
+
+        private void BtnMerge_Click(object sender, EventArgs e)
+        {
+            if (FileList.Items.Count <= 1) return;
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF files (*.pdf)|*.pdf";
+            saveFileDialog.Title = "Сохранить объединенный PDF файл";
+            saveFileDialog.ShowDialog();
+
+            if (!string.IsNullOrWhiteSpace(saveFileDialog.FileName))
+            {
+                string outputPath = saveFileDialog.FileName;
+
+                try
+                {
+                    PdfDocument outputDocument = new PdfDocument();
+
+                    foreach (var pdfFile in this.files)
+                    {
+                        PdfDocument inputDocument = PdfReader.Open(pdfFile.Path, PdfDocumentOpenMode.Import);
+
+                        foreach (PdfPage page in inputDocument.Pages)
+                        {
+                            outputDocument.AddPage(page);
+                        }
+
+                        inputDocument.Close();
+                    }
+
+                    outputDocument.Save(outputPath);
+
+                    MessageBox.Show("PDF файлы успешно объединены и сохранены в " + outputPath, "Успешно :)");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка при объединении и сохранении PDF файлов: " + ex.Message, "Упс... что-то сломалось.");
+                }
+            }
+        }
+    }
+
+    public class PDF_File
+    {
+        public int Num          { get; set; }
+        public string Path      { get; set; }
+        public string FileName  { get; set; }
+
+        public PDF_File(int _num, string _path, string _filename)
+        {
+            this.Num = _num;
+            this.Path = _path;
+            this.FileName = _filename;
+        }
+    }
+}
